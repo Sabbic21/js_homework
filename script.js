f052ae91456598ae1e9cf2af77efed7c6e123be7
Dmitry Kondratyev <dmitry@Dmitrijs-MacBook-Pro.local>
2021-10-06 12:26:33 +0300

    cycles added

--- script.js
+++ script.js
@@ -8,14 +8,39 @@ let appData = {
     income: [],
     savings: false,
     timeData: time
-}
+};
 
-let ans1 = prompt('Введите обязательную статью расходов в этом месяце'),
+/*let ans1 = prompt('Введите обязательную статью расходов в этом месяце'),
     ans2 = prompt('Введите сумму'),
     ans3 = prompt('Введите обязательную статью расходов в этом месяце'),
     ans4 = prompt('Введите сумму');
+    
+    appData.expenses.ans1 = ans2;
+    appData.expenses.ans3 = ans4;*/
 
-appData.expenses.ans1 = ans2;
-appData.expenses.ans3 = ans4;
+/*for(let i = 0; i < 2; i++) {
+    let ansa = prompt('Введите обязательную статью расходов в этом месяце'),
+        ansb = prompt('Введите сумму');
+    
+    appData.expenses[ansa] = ansb;
+};*/
 
-alert(appData.budget / 30);
+let i = 0;
+
+/*while(i < 2) {
+    let ansa = prompt('Введите обязательную статью расходов в этом месяце'),
+        ansb = prompt('Введите сумму');
+    
+    appData.expenses[ansa] = ansb;
+    i++; 
+};*/
+
+do {
+    let ansa = prompt('Введите обязательную статью расходов в этом месяце'),
+        ansb = prompt('Введите сумму');
+    
+    appData.expenses[ansa] = ansb;
+    i++;
+} while(i < 2);
+
+alert("Ваш бюджет на день: " + appData.budget / 30);
